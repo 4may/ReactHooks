4c48cc63bf356016e5a163d093d3be10c0de7ef7
4may <naoki.hanzawa.pub@gmail.com>
2020-05-21 16:53:14 +0900

    乱数生成ボタン

--- callbackHook/src/App.js
+++ callbackHook/src/App.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Button from './Button'
 
-const App = () => (
-    <Button />
-)
+const App = () => {
+    const genRandom = () => {
+        return Math.floor(Math.random() * 10)
+    }
+
+    const initRandom = genRandom()
+    const [appText, setAppText] = useState(initRandom)
+
+    const appStyles = {
+        background : "white",
+        margin : "40px",
+        textAlign : "center",
+        fontSize : "40px"
+    }
+
+    //これだと、呼び出されるたびに関数が生成されてしまうため、非効率
+    const btnCallback = e => {
+        console.log("click")
+        const random = genRandom()
+        setAppText(random)
+    }
+
+    return(
+        <div style={appStyles}>
+            <div>The random number is: {appText}</div>
+            <Button callback={btnCallback}/>
+        </div>
+    )
+}
 
 export default App
--- callbackHook/src/Button.js
+++ callbackHook/src/Button.js
@@ -1,31 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-const Button = () => {
-    const genRandom = () => {
-        return Math.floor(Math.random() * 10)
-    }
-    
-    const [state, setState] = useState({
-        random : genRandom()
-    })
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-
-        console.log(state.random)
-        setState(prevState => ({random : genRandom()}))
-        console.log("hello")
-    }
-
-    //JSXをreturnする
-    return(
-        <React.Fragment>
-            <p>The random number is: {state.random}</p>
-            <form onSubmit={handleSubmit}>
-                <button>Press me!</button>
-            </form>
-        </React.Fragment>
-    )
+const buttonStyle = {
+    width : "200px",
+    padding : "20px",
+    background : "black",
+    color : "white",
+    fontSize : "20px",
+    margin : "40px"
 }
+
+const Button = ({ callback }) => (
+    //JSXをreturnする
+    <button style={buttonStyle} onClick={callback}>
+        {console.log("button re-rendered!")}
+        Press me!
+    </button>
+)
 
 export default Button
